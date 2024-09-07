b15815cffb48686a63f81aa28f58c9b2dc1aaf88
AYUSH PATHAK <92669182+AYUSH-PATHAK-1@users.noreply.github.com>
2024-09-07 15:54:50 +0530

    Add files via upload

---
+++ shadcn_trial/src/app/Hello/page.tsx
@@ -0,0 +1,145 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { toast } from "sonner";
+
+import React from "react";
+
+const hello = () => {
+  return (
+    <>
+      <div className=" h-screen flex justify-center  items-center border-2">
+        <div className=" items-center flex flex-col gap-6">
+          <h1 className=" text-4xl">km chho</h1>
+          <button className=" rounded-2xl bg-blue-500 p-4 text-white font-bold hover:bg-green-800">
+            Test Button
+          </button>
+          <Button variant={"secondary"} size={"lg"}>
+            Shadcn Button
+          </Button>
+          <div className=" w-[500px] p-4 h-full">
+            <Accordion type="single" collapsible>
+              <AccordionItem value="item-1">
+                <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                <AccordionContent>
+                  Yes. It adheres to the WAI-ARIA design pattern.
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
+          <AlertDialog>
+            <AlertDialogTrigger>Open</AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction>Continue</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+          <br />
+          <br />
+          {/* // 33% of the carousel width. */}
+          <Carousel
+            opts={{
+              align: "start",
+            }}
+            orientation="vertical"
+            className="w-full max-w-xs">
+            <CarouselContent className="-mt-1 h-[200px]">
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index} className="pt-1 md:basis-1/2">
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex items-center justify-center p-6">
+                        <span className="text-3xl font-semibold">
+                          {index + 1}
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+          <br />
+          <br />
+          <Drawer>
+            <DrawerTrigger>Open</DrawerTrigger>
+            <DrawerContent>
+              <DrawerHeader>
+                <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                <DrawerDescription>
+                  This action cannot be undone.
+                </DrawerDescription>
+              </DrawerHeader>
+              <DrawerFooter>
+                <Button>Submit</Button>
+                <DrawerClose>
+                  <Button variant="outline">Cancel</Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+          <Button
+            variant="outline"
+            onClick={() =>
+              toast("Event has been created", {
+                description: "Sunday, December 03, 2023 at 9:00 AM",
+                action: {
+                  label: "Undo",
+                  onClick: () => console.log("Undo"),
+                },
+              })
+            }>
+            Show Toast
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default hello;
